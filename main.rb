427cfe514a7e6ae140eeabff021cec5ef3e808da
hyunho <oksknoh@gmail.com>
2012-03-12 19:21:24 +0900

    I wanna add some test, but not completed. this is temporary commit.

--- main.rb
+++ main.rb
@@ -1,20 +1,34 @@
 require 'compress'
 
 
+
 allowed_error = 3
 
-data = [1,2,3,4,5,6,6,7,2,3,4,23,5,6,36, 10]
 
+
+
+data_size = 128
+data =  data_size.times.map { 20 + rand(5)}
 
 regression_cofficient =  Compression.regression(data)
+
 p Compression.max_error(data, Compression.inverse_regression(regression_cofficient,8))
 
-
-
 haar_cofficient = Compression.haar_transform(data)
+haar_cofficient = haar_cofficient[0..(haar_cofficient.size/2-1)] + Array.new(haar_cofficient.size/2,0)
+
 p Compression.max_error(data, Compression.inverse_haar_transform(haar_cofficient))
 
-data =  (1..(128)).map {|b|"a"}
+
+
+
+def evalutation_test data
+  if e(haar(data)) > e(regression(data))
+    puts 'haar wins'
+  else
+    puts 'regression wins'
+  end
+end
 
 
 
